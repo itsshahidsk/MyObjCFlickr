0d2a7e8747a19e9927f832ecfd4f499e5ab5675b
Shahid <shahid@prolifiq.com>
2016-06-18 14:34:54 -0700

    Initial Commit

---
+++ MyObjectiveFlickr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyObjectiveFlickr
+//
+//  Created by Shahid on 6/18/16.
+//  Copyright © 2016 Sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyObjectiveFlickr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyObjectiveFlickr
+//
+//  Created by Shahid on 6/18/16.
+//  Copyright © 2016 Sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
